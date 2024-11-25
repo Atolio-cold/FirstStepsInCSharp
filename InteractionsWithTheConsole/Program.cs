a583762151eb2d3bcd1803db5b6e3c4fe31bb9fd
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-25 20:16:52 +0100

    Add files via upload

--- InteractionsWithTheConsole/Program.cs
+++ InteractionsWithTheConsole/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FirstProrject
+{
+
+    internal class Program
+    {
+
+        private static void Main()
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
