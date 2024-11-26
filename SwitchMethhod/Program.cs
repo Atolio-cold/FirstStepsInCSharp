3b3b2c6e1de778fb140db8e3bda6162fa84d10aa
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-27 00:29:45 +0100

    Add files via upload

--- SwitchMethhod/Program.cs
+++ SwitchMethhod/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Number
+{
+    public class IsEvenNumber
+    {
+
+        public static bool EvenNumber(int number)
+        {
+
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+        public static void Main()
+        {
+
+            Console.WriteLine($"Liczba 1 jest parzysta: {EvenNumber(1)}"); // false
+
+
+            Console.WriteLine($"Liczba 4 jest parzysta: {EvenNumber(4)}"); // true
+
+
+            Console.WriteLine($"Liczba 6 jest parzysta: {EvenNumber(6)}"); // true
+        }
+
+
+    }
+}
