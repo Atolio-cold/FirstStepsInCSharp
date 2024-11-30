6b036c05080a560a483bb1786416d085c3ea041d
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-30 18:18:37 +0100

    Add files via upload

---
+++ EnumBasicTask/Age.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProject
+{
+
+    enum Age : byte
+    {
+        Over18 = 1,
+        Least18 = 2
+    }
+}
---
+++ EnumBasicTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace FirstProject
+{
+
+    public class Program
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("How old are u?\n1 = if u are older than 18, 2 = if u are younger than 18  ");
+            string userInput = Console.ReadLine();
+
+            Age userAge = (Age)Enum.Parse(typeof(Age), userInput);
+
+            if (userAge == Age.Least18)
+            {
+                Console.WriteLine("You are too young ");
+            }
+            else
+            {
+                Console.WriteLine("Hi");
+            }
+
+
+        }
+
+    }
+}
+
