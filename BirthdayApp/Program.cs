1cc000c12c7c34733d93957f0cc083e153a794e1
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-01 17:03:22 +0100

    Add files via upload

---
+++ BirthdayApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace BirthdayApp
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Wprowadź datę w formacie RRRR-MM-DD");
+            string userInput = Console.ReadLine();
+
+            if (DateTime.TryParse(userInput, out DateTime Birth))
+            {
+                DateTime DateNow = DateTime.Now;
+
+                TimeSpan difference = DateNow - Birth;
+
+                Console.WriteLine($"Od twoich dni mineło: {difference.Days} ");
+            }
+            else
+            {
+                Console.WriteLine("Niepoprawny format daty. Spróbuj ponownie.");
+            }
+        }
+    }
+}
