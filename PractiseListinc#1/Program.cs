7b4faa9a6b9d27ffb535b67cb8df06a79cc84bdb
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-09 19:43:04 +0100

    Add files via upload

---
+++ PractiseListinc#1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public class TaskManager
+    {
+        private List<string> tasks = new List<string>();
+
+        public void AddTask(string task)
+        {
+            if (!string.IsNullOrEmpty(task))
+            {
+                tasks.Add(task);
+                Console.WriteLine($"Task '{task}' added.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid task. Task cannot be empty.");
+            }
+        }
+
+
+        public void RemoveTask(string task)
+        {
+            if (tasks.Contains(task))
+            {
+                tasks.Remove(task);
+                Console.WriteLine($"Task '{task}' removed.");
+            }
+            else
+            {
+                Console.WriteLine($"Task '{task}' not found.");
+            }
+        }
+
+        public List<string> GetTasks()
+        {
+            return tasks;
+        }
+    }
+}
