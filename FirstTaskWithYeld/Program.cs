c7d22cc1f3210f4fea64bee9d11cc5e72662ce1b
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-18 18:29:25 +0100

    Add files via upload

---
+++ FirstTaskWithYeld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    using System.Collections.Generic;
+
+    public class EvenNumberGenerator
+    {
+        public IEnumerable<int> GenerateEvenNumbers()
+        {
+            int current = 0;
+            while (true)
+            {
+                // Kod w środku pętli
+                yield return current;
+                current += 2;
+                
+            }
+
+
+        }
+    }
+
+}
