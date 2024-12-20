c53777311eea0e8be73f7afadba80ce20b49a71d
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-20 20:05:36 +0100

    Add files via upload

--- Coding.ExerciseTemperatureConverter/Program.cs
+++ Coding.ExerciseTemperatureConverter/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection;
+
+namespace Coding.Exercise
+{
+    public class TemperatureConverter
+    {
+        public void ConvertCelsiusToFahrenheit(double celsius, out double fahrenheit)
+        {
+            // todo
+            fahrenheit = (celsius * 9 / 5) + 32;
+        }
+
+        public void ConvertFahrenheitToCelsius(double fahrenheit, ref double celsius)
+        {
+            // todo
+            celsius = (fahrenheit - 32) * 5 / 9;
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            var converter = new TemperatureConverter();
+
+            double fahrenheitResult;
+            converter.ConvertCelsiusToFahrenheit(25, out fahrenheitResult);
+            Console.WriteLine($"25°C to {fahrenheitResult}°F");
+
+            double celsiusResult = 0;
+            converter.ConvertFahrenheitToCelsius(77, ref celsiusResult);
+            Console.WriteLine($"77°F to {celsiusResult}°C");
+        }
+    }
+
+}
