b0ec5675205b9220ac8bd70be7bf8a03e1d43e1c
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-22 17:19:58 +0100

    Add files via upload

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Zgadnij liczbę od 1 do 10:");
+
+            Random random = new Random();
+            int rangeNumber = random.Next(1, 11);
+
+            while (true)
+            {
+                Console.Write("Podaj swoją liczbę: ");
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int userInputInt))
+                {
+                    if (userInputInt == rangeNumber)
+                    {
+                        Console.WriteLine("Congratulations! Zgadłeś liczbę!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Źle! Spróbuj ponownie.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("To nie jest liczba! Podaj poprawną liczbę.");
+                }
+            }
+        }
+    }
+}
