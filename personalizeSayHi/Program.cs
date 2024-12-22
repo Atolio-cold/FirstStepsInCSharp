c83a0f3238c6d6d35aa5d8bfd1cfca30a875422d
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-22 18:56:30 +0100

    Add files via upload

---
+++ personalizeSayHi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace personalizeSayHi
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Insert your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Insert your  age: ");
+            string age = Console.ReadLine();
+            int ageUserInput = int.Parse(age);
+
+            if (ageUserInput < 0)
+            {
+                Console.WriteLine("incorrect age");
+            }
+
+            else if (ageUserInput > 50)
+            {
+                Console.WriteLine("Wow, you're really old!");
+            }
+            else
+            {
+                Console.WriteLine("You are young man");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine($"Hi, {name}, Yur're {ageUserInput}");
+
+
+        }
+    }
+}
